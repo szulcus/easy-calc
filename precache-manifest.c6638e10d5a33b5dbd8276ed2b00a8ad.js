53a433ad7c3d315fd5df0bf17cfc09eb4f726b75
jakub104 <jakubzlezena@wp.pl>
2020-02-29 22:29:42 +0100

    Updates

---
+++ precache-manifest.c6638e10d5a33b5dbd8276ed2b00a8ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a63950b9668fff0675ba8d271529e5",
+    "url": "/easy-calc/index.html"
+  },
+  {
+    "revision": "e673a52dd3c7eb863b4d",
+    "url": "/easy-calc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "308e8fe8c1cd919aa2cb",
+    "url": "/easy-calc/static/js/2.a99d2516.chunk.js"
+  },
+  {
+    "revision": "f1a03c1f2e879f3cba3e2b57eabb23eb",
+    "url": "/easy-calc/static/js/2.a99d2516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e673a52dd3c7eb863b4d",
+    "url": "/easy-calc/static/js/main.6518a674.chunk.js"
+  },
+  {
+    "revision": "9d978cb7d893d48a76ad",
+    "url": "/easy-calc/static/js/runtime-main.792bedf6.js"
+  }
+]);
