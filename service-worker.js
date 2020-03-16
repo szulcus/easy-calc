7c363b1e14fc7142017a662fa5196d968b42988a
jakub104 <jakubzlezena@wp.pl>
2020-03-16 18:15:59 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/easy-calc/precache-manifest.2fb223fe7077a69c1a03b1ae639919e9.js"
+  "/easy-calc/precache-manifest.bdf8e17850daccca8d3b2b0b5ca01601.js"
 );
 
 self.addEventListener('message', (event) => {
